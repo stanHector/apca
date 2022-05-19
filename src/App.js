7d8e2b7a40d24250d901223656c90a86e7f78ed3
Hector <63668294+stanHector@users.noreply.github.com>
2022-05-19 10:15:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
+import React from 'react'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Education from "./components/education/Education";
-// import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-// import Education from "./components/education/Education"
-import CreateIndicator from './components/indicator/CreateIndicator'
-import Policy from "./components/policy/Policy";
-import ServiceDelivery from './components/serviceDelivery/ServiceDelivery'
-import Topbar from './components/topbar/Topbar'
+
+import Topbar from "./components/topbar/Topbar"
 
 function App() {
+
   return (
-    <div className="App">
+    <div className='App'>
       <Topbar/>
-     <Education/>
     </div>
   );
 }
