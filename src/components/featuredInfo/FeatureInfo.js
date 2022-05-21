20beabfef4c4f16b93f769445832d4710c574b34
Hector <stanleychike@yahoo.com>
2022-05-21 21:49:47 +0100

    adding colors to cards

--- src/components/featuredInfo/FeatureInfo.js
+++ src/components/featuredInfo/FeatureInfo.js
@@ -12,49 +12,44 @@ export default function FeatureInfo() {
         <>
 
             <div className='featured'>
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#FEF5E7" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
 
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#FAD7A0" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#EDBB99" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#F5B041" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#F1C40F" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
-                <div className="featuredItem" style={{ backgroundColor: "#0EEEBE" }}>
+                <div className="featuredItem" style={{ backgroundColor: "#E59866" }}>
                     <span className="featuredTitle"> Users</span>
                     <div className="text-right" style={{ textAlign: "right", marginTop: "45px" }}>
                         <span className="featuredMoney">#</span>
                     </div>
                 </div>
-
-
             </div>
-
-
-
         </>
     )
 }
